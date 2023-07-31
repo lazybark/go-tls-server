f74eb75538f117c20bd06e60a8f4dbd9e15af3d0
Serhii <lazybark.dev@gmail.com>
2023-07-31 22:10:21 +0300

    Stats no connection + StatsConnections

--- v3/server/stat.go
+++ v3/server/stat.go
@@ -7,27 +7,45 @@ import (
 
 // Stat holds server statistic
 type Stat struct {
-	recieved    int
-	sent        int
-	errors      int
-	connections int
+	recieved int
+	sent     int
+	errors   int
 }
 
+var ErrNoStatForTheDay = fmt.Errorf("no stat")
+
+const statKeyPattern = "%d-%d-%d"
+
 // Stats returns server stats for specified day or error in case date is not in stat
-func (s *Server) Stats(y int, m int, d int) (int, int, int, int, error) {
-	date := fmt.Sprintf("%d-%d-%d", y, m, d)
+func (s *Server) Stats(y int, m int, d int) (sentBytes, recievedBytes, errors int, err error) {
+	date := fmt.Sprintf(statKeyPattern, y, m, d)
 
 	s.statMutex.Lock()
 	defer s.statMutex.Unlock()
 	if v, ok := s.Stat[date]; ok {
-		return v.sent, v.recieved, v.errors, v.connections, nil
+		return v.sent, v.recieved, v.errors, nil
 	}
-	return 0, 0, 0, 0, fmt.Errorf("no stat")
+
+	err = ErrNoStatForTheDay
+	return
+}
+
+// StatsConnections returns statistic about current connections
+func (s *Server) StatsConnections() (connections int, err error) {
+	//Right now err is not used - added for compatibility for future
+	s.connPoolMutex.Lock()
+	connections = len(s.connPool)
+	s.connPoolMutex.Unlock()
+
+	return
 }
 
 // addRecBytes adds bytes to stat of current day
 func (s *Server) addRecBytes(n int) {
-	d := fmt.Sprintf("%d-%d-%d", time.Now().Year(), time.Now().Month(), time.Now().Day())
+	if n < 0 {
+		return
+	}
+	d := getStatKey()
 
 	s.statMutex.Lock()
 	defer s.statMutex.Unlock()
@@ -42,7 +60,10 @@ func (s *Server) addRecBytes(n int) {
 
 // addSentBytes adds bytes to stat of current day
 func (s *Server) addSentBytes(n int) {
-	d := fmt.Sprintf("%d-%d-%d", time.Now().Year(), time.Now().Month(), time.Now().Day())
+	if n < 0 {
+		return
+	}
+	d := getStatKey()
 
 	s.statMutex.Lock()
 	defer s.statMutex.Unlock()
@@ -56,7 +77,10 @@ func (s *Server) addSentBytes(n int) {
 
 // addErrors adds bytes to stat of current day
 func (s *Server) addErrors(n int) {
-	d := fmt.Sprintf("%d-%d-%d", time.Now().Year(), time.Now().Month(), time.Now().Day())
+	if n < 0 {
+		return
+	}
+	d := getStatKey()
 
 	s.statMutex.Lock()
 	defer s.statMutex.Unlock()
@@ -66,4 +90,9 @@ func (s *Server) addErrors(n int) {
 	} else {
 		s.Stat[d] = Stat{errors: n}
 	}
+}
+
+func getStatKey() string {
+	now := time.Now()
+	return fmt.Sprintf(statKeyPattern, now.Year(), now.Month(), now.Day())
 }
